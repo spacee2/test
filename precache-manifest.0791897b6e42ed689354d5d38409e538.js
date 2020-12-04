b8ea2e95f4be40c9e51dac13042991521dd13c1d
ecl5pse <fantmas20@gmail.com>
2020-12-04 19:31:17 +0200

    first commit

---
+++ precache-manifest.0791897b6e42ed689354d5d38409e538.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713c5de8ba97366f71735aafe63a0aba",
+    "url": "/typography/index.html"
+  },
+  {
+    "revision": "2ae7caa07f256e7c0270",
+    "url": "/typography/static/css/2.2fc49fc9.chunk.css"
+  },
+  {
+    "revision": "609cf53af455430eb4e9",
+    "url": "/typography/static/css/main.6e7e5d6d.chunk.css"
+  },
+  {
+    "revision": "2ae7caa07f256e7c0270",
+    "url": "/typography/static/js/2.9db89d98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typography/static/js/2.9db89d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609cf53af455430eb4e9",
+    "url": "/typography/static/js/main.8f0a25d4.chunk.js"
+  },
+  {
+    "revision": "0bb2b6357f70b415e0e1",
+    "url": "/typography/static/js/runtime-main.ab84d4f8.js"
+  },
+  {
+    "revision": "0b1e5c66ff3a8182adf594c54f08f630",
+    "url": "/typography/static/media/logo.0b1e5c66.png"
+  },
+  {
+    "revision": "e37f9c73bdb14c49757226004131f002",
+    "url": "/typography/static/media/unnamed.e37f9c73.jpg"
+  },
+  {
+    "revision": "3f1913add7aa2de0bcd8362ad639f33f",
+    "url": "/typography/static/media/unnamed1.3f1913ad.jpg"
+  }
+]);
